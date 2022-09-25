47580e8cf55365c3836de6144d2c1b973b48a860
nidaloldd <mindemes@gmail.com>
2022-09-25 14:01:04 +0200

    javadoc

--- src/main/java/Model/PriorityLevel.java
+++ src/main/java/Model/PriorityLevel.java
@@ -1,5 +1,11 @@
 package Model;
 
+/**
+ * This enum Class contains the PriorityLevels that a Cetli can get.
+ * Red mean a very important task.
+ * Yellow mean a moderately  important task.
+ * Green mean a not so important task.
+ */
 public enum PriorityLevel {
 
     Red,
